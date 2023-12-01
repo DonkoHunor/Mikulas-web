dcc40b2c3cca23b98aa6b01ad90814fce54e6334
Petrik Diak <diak@petrik.hu>
2023-12-01 11:38:11 +0100

    Órai munka

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,15 @@
-import { Controller, Get, Render } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Body, Controller, Get, Param, Post, Render } from '@nestjs/common';
 import { AppService } from './app.service';
+import * as mysql from 'mysql2';
+import { ujGyerekDTO } from './ujGyerekDTO';
+
+const connection = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'mikulas'  
+}).promise();
 
 @Controller()
 export class AppController {
@@ -7,7 +17,35 @@ export class AppController {
 
   @Get()
   @Render('index')
-  index() {
-    return { message: 'Welcome to the homepage' };
+  async index() {
+    const [adatok,mezok] = await connection.execute('SELECT id, nev, jo, ajandek FROM gyerekek');
+    console.log(adatok);
+    console.log(mezok);
+    return { gyerekek: adatok };
+  }
+
+  @Get('gyerekek/:id')
+  @Render('gyerek')
+  async egyGyerek(@Param('id') id: number) {
+    const [adatok] = await connection.execute('SELECT nev, jo, ajandek FROM gyerekek WHERE id=?', [id]);
+    return adatok[0];
+  }
+
+  @Post('/ujGyerek')
+  @Render('ujGyerek')
+  async ujGyerek(@Body() ujGyerek: ujGyerekDTO) {
+    const nev = ujGyerek.nev
+    const jo: boolean = ujGyerek.jo == '1';
+    const ajandek = jo ? ujGyerek.ajandek : null;
+
+   const [adatok] = await connection.execute('INSERT INTO gyerekek (nev,jo, ajandek) VALUES (?,?,?)',[nev, jo, ajandek]);
+   console.log(adatok);
+   return{};
+  }
+
+  @Get('/ujGyerek')
+  @Render('ujGyerek')
+  ujGyerekForm() {
+    return{};
   }
 }
---
+++ src/ujGyerekDTO.ts
@@ -0,0 +1,6 @@
+/* eslint-disable prettier/prettier */
+export class ujGyerekDTO {
+  nev: string;
+  jo: string;
+  ajandek: string
+}
